29a1abd4e66d86a93c05f51da1392bae0772e1bd
Douglas Gass <douglasgass2@hotmail.com>
2019-11-09 18:32:12 -0300

    Initial project
    Start project with spring initializr

---
+++ src/main/java/com/service/managevotingsessionsservice/ManageVotingSessionsServiceApplication.java
@@ -0,0 +1,13 @@
+package com.service.managevotingsessionsservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManageVotingSessionsServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManageVotingSessionsServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/service/managevotingsessionsservice/ManageVotingSessionsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.service.managevotingsessionsservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManageVotingSessionsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
