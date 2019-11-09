63605b8b01a4aa3b5c8c6739c6bd39d1490acacb
Douglas Gass <douglasgass2@hotmail.com>
2019-11-09 19:15:44 -0300

    Configure feign client, mongo and gitignore

--- src/main/java/com/service/managevotingsessionsservice/ManageVotingSessionsServiceApplication.java
+++ src/main/java/com/service/managevotingsessionsservice/ManageVotingSessionsServiceApplication.java
@@ -2,8 +2,12 @@ package com.service.managevotingsessionsservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.data.mongodb.repository.config.EnableReactiveMongoRepositories;
 
 @SpringBootApplication
+@EnableReactiveMongoRepositories
+@EnableFeignClients
 public class ManageVotingSessionsServiceApplication {
 
 	public static void main(String[] args) {
