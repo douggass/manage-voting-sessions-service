9278534d591069c493ced4dd5c5486a3e30fb843
Douglas Gass <douglasgass2@hotmail.com>
2019-11-10 10:40:38 -0300

    Create exception to handling if no sessionId in database

---
+++ src/main/java/com/service/managevotingsessionsservice/exception/ApiNoDataException.java
@@ -0,0 +1,7 @@
+package com.service.managevotingsessionsservice.exception;
+
+public class ApiNoDataException extends RuntimeException {
+	public ApiNoDataException(String message) {
+		super(message);
+	}
+}
